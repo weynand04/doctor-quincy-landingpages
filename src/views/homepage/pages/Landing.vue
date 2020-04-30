ab9d3ca88a367939c2c4e69e156d3ec3822836b6
Weynand <wamaerkabor@gmail.com>
2020-04-30 22:20:05 +0700

    Recovery assets style

--- src/views/homepage/pages/Landing.vue
+++ src/views/homepage/pages/Landing.vue
@@ -1,20 +1,24 @@
 <template>
-  <v-container>
-    <v-row class="col-12 d-flex justify-space-around">
-      <v-col md="4" sm="12">
+  <v-container id="landing" tag="section">
+    <v-row class="col-12" style="backgorund-color:#FFFF">
+      <v-col md="4">
         <v-card color="transparent" flat>
-          <v-card-title class="v-card__title display-3">Web Design Solution for our Doctors</v-card-title>
-          <v-card-subtitle class="v-card__subtitle mt-5" style="font-size:25px;">Be Our Partner</v-card-subtitle>
-          <v-card-actions class="v-card__actions">
+          <v-card-title class="display-2 font-weight-black black--text">
+            Web Solutions
+            <br />Designed for our
+            <br />Doctors
+          </v-card-title>
+          <v-card-subtitle class="title font-weight-regular pt-3">Be Our Patner</v-card-subtitle>
+          <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn rounded outlined color="#F8B218">PARTNER</v-btn>
             <v-btn rounded color="#F8B218">MEMBER</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col md="8" sm="12">
-        <v-card class="mt-0" flat>
-          <v-img src="@/assets/image/undraw_doctors_hwty.svg" aspect-ratio="1.4" contain></v-img>
+      <v-col>
+        <v-card color="transparent" flat>
+          <v-img src="@/assets/image/undraw_doctors_hwty.svg" aspect-ratio="1.7" contain />
         </v-card>
       </v-col>
     </v-row>
@@ -26,15 +30,3 @@ export default {
   name: "Home"
 };
 </script>
-
-<style lang="scss">
-.v-card {
-  .v-card__title {
-    text-align: left;
-    font: Bold 50px/75px Corbel;
-    letter-spacing: 0px;
-    color: #000000;
-    opacity: 1;
-  }
-}
-</style>
