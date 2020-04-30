adeece602250324b0656bd3a9adbe2ff68ce7a01
Weynand <wamaerkabor@gmail.com>
2020-04-30 16:52:43 +0700

    Update Appbar / header

--- src/views/homepage/components/core/AppBar.vue
+++ src/views/homepage/components/core/AppBar.vue
@@ -1,54 +1,60 @@
   <template>
-  <v-app-bar app elevate-on-scroll color="white" class="appbar">
-    <!-- logo -->
-    <v-toolbar-title>
-      <a href="/">
-        <v-img
-          src="@/assets/logo.jpg"
-          class="logo"
-          aspect-ratio="1.7"
-          height="52"
-          width="199"
-          contain
-        ></v-img>
-      </a>
-    </v-toolbar-title>
+  <v-app-bar app elevate-on-scroll color="#FFFF">
+    <v-container>
+      <v-toolbar flat class="v-toolbar">
+        <!-- logo -->
+        <!-- <v-app-bar-nav-icon class="d-none d-sm-flex d-md-none"></v-app-bar-nav-icon> -->
+        <v-toolbar-title class="d-none d-sm-none d-md-flex">
+          <a href="/">
+            <v-img
+              src="@/assets/logo.jpg"
+              class="logo"
+              aspect-ratio="1.7"
+              height="52"
+              width="199"
+              contain
+            ></v-img>
+          </a>
+        </v-toolbar-title>
 
-    <!-- menu mitra dan Konseling -->
-    <v-menu offset-y transition="scale-transition">
-      <template v-slot:activator="{ on }">
-        <v-btn text class="font-weight-bold" to="help">Konseling</v-btn>
-        <v-btn text v-on="on" class="font-weight-bold">
-          Jadilah Mitra kami
-          <v-icon icon>mdi-chevron-down</v-icon>
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item v-for="(item, index) in items" :key="index">
-          <v-list-item-title class="fucek">
-            <a href="#">{{ item.title }}</a>
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-    <v-spacer />
+        <!-- menu mitra dan konseling -->
+        <v-toolbar-items class="d-none d-sm-none d-md-flex">
+          <v-menu offset-y transition="scale-transition">
+            <template v-slot:activator="{ on }">
+              <v-btn text v-on="on">Jadilah Mitra kami</v-btn>
+              <v-btn text to="/help">Konseling</v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title>
+                  <a href="#">{{item.title}}</a>
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
 
-    <!-- menu expand -->
-    <v-menu offset-x offset-y left transition="slide-y-transition" v-model="menu">
-      <template v-slot:activator="{ on }">
-        <v-btn v-on="on" icon>
-          <v-icon large>mdi-menu</v-icon>
-          <!-- <v-icon v-else>mdi-close</v-icon> -->
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item v-for="(Menuitem, index) in Menuitem" :key="index">
-          <v-list-item-title class="fucek">
-            <a>{{ Menuitem.title }}</a>
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+        <!-- menu expand -->
+        <v-toolbar-items>
+          <v-menu offset-x offset-y left transition="slide-y-transition" v-model="menu">
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on" icon>
+                <v-icon large>mdi-menu</v-icon>
+                <!-- <v-icon v-else>mdi-close</v-icon> -->
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="(Menuitem, index) in Menuitem" :key="index">
+                <v-list-item-title class="fucek">
+                  <a>{{ Menuitem.title }}</a>
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-container>
   </v-app-bar>
 </template>
 
@@ -76,11 +82,4 @@ export default {
 </script>
 
 <style>
-.appbar {
-  padding-left: 65px !important;
-  padding-right: 70px !important;
-}
-.fucek a {
-  color: black;
-}
 </style>
