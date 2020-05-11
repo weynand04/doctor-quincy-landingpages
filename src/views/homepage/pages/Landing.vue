21b1da272e4fddb8e65f3e3486305775418fe748
haris.tj <tanoneharis@gmail.com>
2020-05-11 09:14:09 +0700

    add new branch health-care

--- src/views/homepage/pages/Landing.vue
+++ src/views/homepage/pages/Landing.vue
@@ -5,7 +5,7 @@
       <v-col md="4" sm="12">
         <v-card flat class="pl-12 ml-8">
           <v-card-title
-            class="display-1 font-weight-bold black--text text-break"
+          class="display-1 font-weight-bold black--text text-break"
           >Web Solutions Designed for our Doctors</v-card-title>
           <v-card-subtitle class="title font-weight-regular pt-3">Be Our Patner</v-card-subtitle>
           <v-card-actions>
@@ -17,10 +17,10 @@
       </v-col>
       <v-col md="8" sm="12">
         <v-img
-          src="@/assets/image/undraw_doctors_hwty.svg"
-          aspect-ratio="1.6"
-          contain
-          position="center"
+        src="@/assets/image/undraw_doctors_hwty.svg"
+        aspect-ratio="1.6"
+        contain
+        position="center"
         ></v-img>
       </v-col>
     </v-row>
@@ -33,7 +33,7 @@
       <v-col md="8" sm="12" align-self="start">
         <v-card height="200" flat>
           <v-card-title
-            class="font-weight-black orange--text darken-3"
+          class="font-weight-black orange--text darken-3"
           >Kamu Akan Jauh Merasa Lebih Baik Dengan Dokter Quincy</v-card-title>
           <v-window v-model="onboarding" reverse>
             <v-window-item v-for="n in length" :key="n.text">
@@ -62,13 +62,13 @@
         <v-card width="598" flat>
           <v-card-text>
             <p
-              class="body-1 font-weight-bold red--text"
+            class="body-1 font-weight-bold red--text"
             >Dokter Quincy Juga Menawarkan Program “CrystalCare” Generasi-Baru Perawatan Kesehatan Primer Untuk Pegawai Pengusahaan & Pegawai Negeri</p>
             <p>
               <a
-                class="blue--text"
-                href="http://"
-                target="_blank"
+              class="blue--text"
+              href="http://"
+              target="_blank"
               >Klik disini untuk detail lebih lanjut</a>
             </p>
           </v-card-text>
@@ -79,12 +79,12 @@
     <v-row>
       <v-col cols="12" style="background-color:#F8F8F8;" class="d-flex justify-space-around">
         <p
-          class="text-center col-6"
+        class="text-center col-6"
         >Bukankah Sudah Saatnya Kamu Benar-Benar Mulai Memikirkan Kembali Cara Kamu Menerima Perawatan Kesehatan?</p>
       </v-col>
       <v-col cols="12" style="background-color:#6B6B6B" class="d-flex justify-space-around">
         <p
-          class="text-center white--text col-8"
+        class="text-center white--text col-8"
         >Pendekatan kami terhadap perawatan kesehatan adalah membuatnya mudah dan bebas repot untuk semua orang,meruntuhkan penghalang birokrasi dengan mendukung kesehatan kamu dimanapun kamu berada. Dokter dan psikolog berlisensi kami tersedia 24 jam setiap hari.</p>
       </v-col>
     </v-row>
@@ -94,18 +94,18 @@
         <v-col class="d-flex justify-space-around">
           <v-card width="350">
             <v-card-title
-              class="font-weight-bold orange--text darken-3 text-left text-break"
+            class="font-weight-bold orange--text darken-3 text-left text-break"
             >Perawatan Kesehatan Yang Berkualitas Dimulai Dengan Dokter Yang Berkualitas</v-card-title>
             <v-card-text
-              class="body-1 font-weight-light"
+            class="body-1 font-weight-light"
             >Kami menjamin kesehatan kamu dari A hingga Z dan Dokter Quincy disini untuk mendukung kesejahteraan kamu seiring waktu. Dari dokter hingga psikolog, kami membantu mendukung perjalanan kesehatan kamu. Tim dokter kami yang penuh kasih dan tepercaya berasal dari berbagai latar belakang dan spesialisasi. Mereka akan menciptakan jenis hubungan baru dengan kamu dengan meluangkan waktu untuk mendengarkan dan memperhatikan detail masalah kesehatan kamu. Dokter kami sedang merevolusi akses layanan kesehatan yang berkualitas. Mereka dilatih secara profesional untuk menggunakan teknologi virtual untuk menangani banyak kondisi non-darurat. Tim ahli medis kami adalah para dokter dengan pengalaman bertahun-tahun.</v-card-text>
           </v-card>
           <v-card width="350">
             <v-card-title
-              class="font-weight-bold orange--text darken-3 text-left text-break"
+            class="font-weight-bold orange--text darken-3 text-left text-break"
             >Biaya Terjangkau Dan Transparan</v-card-title>
             <v-card-text
-              class="body-1 font-weight-light"
+            class="body-1 font-weight-light"
             >Layanan Doctor Quincy tersedia secara nasional dan internasional, dengan atau tanpa asuransi. Gratis untuk bergabung, dan biaya dokter kunjungan kamu selalu ditampilkan di muka sehingga kamu tahu berapa biayanya, tanpa tagihan kejutan nanti. Kami bekerja dengan asuransi lainnya termasuk BPJS, pegawai pengusahaan dan pegawai negeri. Doctor Quincy menawarkan akses kesehatan yang terjangkau, transparan, lebih mudah dan tidak merepotkan untuk semua orang. Kami baru saja memulai perjalanan merevolusi layanan kesehatan di Indonesia dan menjadikannya lebih mudah diakses oleh orang-orang di mana saja. Inovasi kesehatan digital mengubah hidup kita menjadi lebih baik dan inovatif, solusi kesehatan generasi baru ini akan membantu memecahkan banyak tantangan dalam perawatan kesehatan primer dan mengatasi kebutuhan pasien, dokter, dan masih banyak lagi.</v-card-text>
           </v-card>
         </v-col>
@@ -119,18 +119,18 @@ export default {
   name: "Home",
   data: () => ({
     length: [
-      {
-        text:
-          "Sekarang kamu dapat memiliki dokter sesuai permintaan dengan mudah dan bebas repot mengunjungi kamu dalam hitungan menit sesuai kenyamanan kamu, hanya dengan menggunakan ponsel cerdas, tablet, atau laptop kamu 24 jam sehari."
-      },
-      {
-        text:
-          "Layanan kesehatan harusnya sederhana, cepat dan tidak rumit. Dokter Quincy memudahkan dokter mengunjungi kamu dalam beberapa menit melalui aplikasi seluler kami dan online."
-      },
-      {
-        text:
-          "Dapatkan akses ke layanan kesehatan berkualitas tanpa harus meninggalkan rumah, pekerjaan, atau dimanapun kamu berada. Betapa mudahnya menerima perawatan yang terjangkau dan berkualitas tanpa bepergian ke rumah sakit! Resep obat dapat dikirim langsung ke apotek jika diperlukan secara medis."
-      }
+    {
+      text:
+      "Sekarang kamu dapat memiliki dokter sesuai permintaan dengan mudah dan bebas repot mengunjungi kamu dalam hitungan menit sesuai kenyamanan kamu, hanya dengan menggunakan ponsel cerdas, tablet, atau laptop kamu 24 jam sehari."
+    },
+    {
+      text:
+      "Layanan kesehatan harusnya sederhana, cepat dan tidak rumit. Dokter Quincy memudahkan dokter mengunjungi kamu dalam beberapa menit melalui aplikasi seluler kami dan online."
+    },
+    {
+      text:
+      "Dapatkan akses ke layanan kesehatan berkualitas tanpa harus meninggalkan rumah, pekerjaan, atau dimanapun kamu berada. Betapa mudahnya menerima perawatan yang terjangkau dan berkualitas tanpa bepergian ke rumah sakit! Resep obat dapat dikirim langsung ke apotek jika diperlukan secara medis."
+    }
     ],
     onboarding: 0
   }),
@@ -138,11 +138,11 @@ export default {
   methods: {
     next() {
       this.onboarding =
-        this.onboarding + 1 === this.length ? 0 : this.onboarding + 1;
+      this.onboarding + 1 === this.length ? 0 : this.onboarding + 1;
     },
     prev() {
       this.onboarding =
-        this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1;
+      this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1;
     }
   }
 };
