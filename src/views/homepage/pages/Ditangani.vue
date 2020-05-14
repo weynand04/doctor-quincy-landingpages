05606008b376bcea1e7e8a11b1b61358ba8341ee
Weynand <wamaerkabor@gmail.com>
2020-05-15 02:33:16 +0700

    add page Penyakit Yang Kami Tangani

--- src/views/homepage/pages/Ditangani.vue
+++ src/views/homepage/pages/Ditangani.vue
@@ -1,16 +1,177 @@
 <template>
   <v-container fluid>
-    <v-card height="500">
-      <v-card-title>Penyakit yang kami tangani</v-card-title>
-      <v-card-actions>
-        <v-btn text to="/">TEST</v-btn>
-      </v-card-actions>
-    </v-card>
+    <!-- section #1 -->
+    <v-row>
+      <v-col cols="12" md="6" align-self="center">
+        <v-card flat class="pl-12 ml-8">
+          <v-card-title
+            class="title1__widu font-weight-bold black--text text-break"
+          >Penyakit Yang Kami Tangani</v-card-title>
+          <v-card-subtitle
+            class="font-weight-medium pt-3"
+          >Kami berkomitmen untuk menawarkan kepada kamu dan keluarga kunjungan dokter terbaik. Daftarkan sekarang dan izinkan dokter-dokter kami membantu dengan lebih dari 200 penyakit baik yang ringan maupun yang berat, mulai dari pilek dan infeksi sinus hingga alergi dan banyak lagi.</v-card-subtitle>
+          <v-card-actions>
+            <v-btn
+              rounded
+              to="/pages/register"
+              class="font-weight-bold elevation-1"
+              color="#F8B218"
+            >LOGIN</v-btn>
+            <v-btn
+              rounded
+              flat
+              to="/pages/register"
+              class="font-weight-bold elevation-1"
+              color="#F8B218"
+            >REGISTER</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-img src="@/assets/image/Group 1031.png" width="562px" height="506px" contain />
+      </v-col>
+    </v-row>
+
+    <!-- section #2  -->
+    <v-row v-for="item in section_2" :key="item.judul" :style="item.bg">
+      <v-col cols="12" class="d-flex justify-space-around">
+        <v-card flat color="transparent" width="90%">
+          <v-card-title class="title1__widu font-weight-bold orange--text">{{item.judul}}</v-card-title>
+          <v-card-text>
+            <p>{{item.teks}}</p>
+          </v-card-text>
+          <v-divider></v-divider>
+        </v-card>
+      </v-col>
+      <v-col cols="12" class="d-flex justify-space-around">
+        <v-card color="transparent" width="90%" class="mt-n9 pt-0">
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-list dense>
+                <v-list-item-group>
+                  <v-list-item v-for="(i, index) in item.list" :key="index">
+                    <v-list-item-icon>
+                      <v-icon>mdi-circle</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>{{i.title}}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-col>
+            <v-col cols="12" md="8" class="mt-3">
+              <p>
+                Ketika salah satu penyakit di sebelah kiri diklik, maka rincian subjek akan muncul
+                <b>DALAM KOTAK INI</b>
+              </p>
+              <p class="red--text">Silakan lihat detail masing-masing penyakit di dalam folder data.</p>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "Ditangani"
+  name: "Ditangani",
+  data: () => ({
+    section_2: [
+      {
+        bg: "background: #FCFCFC",
+        judul: "Perawatan medis",
+        teks:
+          "Ketika kamu atau anggota keluarga sakit dan perlu menemui dokter, tim dokter kami siap siaga setiap saat. Tersedia 24/7, tim penyedia dokter kami dapat membantu kamu bahkan dengan resep obat, jika diperlukan.",
+        list: [
+          {
+            title: "Perawatan Medis"
+          },
+          {
+            title: "Masalah kulit"
+          },
+          {
+            title: "Kesehatan Wanita"
+          },
+          {
+            title: "Kesehatan Pria"
+          }
+        ]
+      },
+      {
+        bg: "background: #F7F7F7",
+        judul: "Perilaku & Jiwa",
+        teks:
+          "Tim kami yang beraneka ragam dari psikolog berlisensi dan psikiater bersertifikat memberikan dukungan emosional yang kamu butuhkan dari privasi dan kemudahan rumah. Dari terapi bicara hingga manajemen pengobatan, tim kami ada disini, siap untuk mendukung kesehatan mental kamu sepenuhnya.",
+        list: [
+          {
+            title: "Perawatan Medis"
+          },
+          {
+            title: "Masalah kulit"
+          },
+          {
+            title: "Kesehatan Wanita"
+          },
+          {
+            title: "Kesehatan Pria"
+          }
+        ]
+      },
+      {
+        bg: "background: #EFEFEF",
+        judul: "Perawatan Kesehatan Preventif",
+        teks:
+          "Tim layanan kesehatan kami yang penuh perhatian ada disini untuk mendukung kesehatan kamu dan rutinitas perawatan-diri sehari-hari. Dari makan sehat hingga pemeriksaan laboratorium preventif, Doctor Quincy menyatukan penyedia tepercaya dengan solusi yang benar-benar bekerja di dunia nyata.",
+        list: [
+          {
+            title: "Kujungan kesehatan"
+          },
+          {
+            title: "Kesehatan Keluarga "
+          },
+          {
+            title: "Kebugaran Wanita"
+          },
+          {
+            title: "Kebugaran Pria"
+          },
+          {
+            title: "Diet & Nutrisi"
+          },
+          {
+            title: "Manajemen Obat-obatan"
+          }
+        ]
+      },
+      {
+        bg: "background: #E9E9E9",
+        judul: "Perawatan Kronis",
+        teks:
+          "Pendekatan kami adalah merawat dan memberi kamu dan keluarga sistem perawatan kesehatan yang fleksibel s aat dibutuhkan. Ketika kamu perlu mengelola kondisi kesehatan yang berkelanjutan atau kronis, Dokter Quincy membuatnya mudah hanya dengan satu sentuhan tombol.",
+        list: [
+          {
+            title: "Asma"
+          },
+          {
+            title: "Kolesterol Tinggi"
+          },
+          {
+            title: "Tekanan Darah Tinggi"
+          },
+          {
+            title: "Manajemen Berat Badan"
+          },
+          {
+            title: "Diabetes"
+          },
+          {
+            title: "Masalah Tiroid"
+          }
+        ]
+      }
+    ]
+  })
 };
 </script>
