301a0886854a521778a6c8e800b51cc1e1515110
Weynand <wamaerkabor@gmail.com>
2020-05-16 05:13:28 +0700

    Update new layout to help

--- src/views/homepage/pages/Help.vue
+++ src/views/homepage/pages/Help.vue
@@ -14,7 +14,7 @@
 }
 </style>
 <template>
-  <v-container fluid>
+  <v-container id="help" fluid>
     <v-row>
       <v-col cols="12" md="6" align-self="center">
         <v-card flat class="pl-12 ml-8">
@@ -22,7 +22,7 @@
             class="title1__widu font-weight-bold black--text text-break"
           >Lorem ipsum dolor sit amet.</v-card-title>
           <v-card-subtitle
-            class="subtitle__widu font-weight-medium pt-3"
+            class="text__widu font-weight-medium pt-3"
           >Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel in dignissimos, incidunt at sint magnam ipsam provident aut ullam. Expedita, eum laudantium doloremque quo corrupti mollitia quaerat deserunt adipisci natus!</v-card-subtitle>
           <v-card-actions>
             <v-btn rounded color="#F8B218">LOGIN</v-btn>
@@ -34,16 +34,95 @@
         <v-img src="@/assets/image/gambar lain-11.png" width="562px" height="506px" contain />
       </v-col>
     </v-row>
+
     <v-row>
-      <v-col cols="12" class="d-flex justify-space-around">
-        <v-card v-for="(n, i) in dt_sec5" :key="i" width="350" class="mr-10">
-          <v-img :src="n.image" class="mygradient" contain>
-            <div class="fill-height mygradient"></div>
-          </v-img>
-          <v-card-title class="orange--text darken-3 text-left text-break">{{n.title}}</v-card-title>
-          <v-card-text class="font-weight-light">{{n.text}}</v-card-text>
+      <v-col align-self="center" class="d-flex justify-space-around">
+        <v-card flat>
+          <v-row align="center">
+            <v-col cols="12" class="d-flex justify-space-around">
+              <v-card v-for="(n, i) in dt_sec5" :key="i" width="350" class="ma-5">
+                <v-img :src="n.image" class="mygradient" contain>
+                  <div class="fill-height mygradient"></div>
+                </v-img>
+                <v-card-title class="orange--text darken-3 text-left text-break">{{n.title}}</v-card-title>
+                <v-card-text class="font-weight-light">{{n.text}}</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
+    </v-row>
+
+    <v-row no-gutters>
+      <v-col cols="12" class="d-flex justify-space-around">
+        <v-card color="transparent" width="88%" flat>
+          <v-card-title
+            class="title1__widu font-weight-bold black--text text-break"
+          >Home Health Care</v-card-title>
+          <v-card-text>
+            <v-img
+              src="@/assets/image/Mask Group 4.png"
+              height="328px"
+              width="389px"
+              class="float-right"
+            />
+            <p
+              class="title font-weight-regular text-break"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus omnis aliquam excepturi magnam eum incidunt blanditiis, velit impedit recusandae repudiandae beatae quisquam fugiat doloribus dolorum labore dolorem perferendis, similique cupiditate libero quas, et quis. Eos, optio modi! Facere aliquid deserunt amet voluptatum eligendi distinctio optio quis deleniti placeat, modi nam?</p>
+            <p
+              class="title font-weight-regular text-break"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officia, asperiores cumque eaque eligendi sit pariatur corporis fugit suscipit aliquid magnam hic excepturi accusamus voluptate! Neque ipsam nostrum sed odio officiis, sequi laudantium dicta laborum sapiente doloremque! Dolores tenetur aut natus ad quos sed alias. Pariatur nemo officiis placeat sequi?</p>
+            <p
+              class="title font-weight-regular text-break"
+            >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam aliquam, laborum tenetur adipisci dolores numquam tempore quos repudiandae libero laudantium atque possimus ipsum aut dolorem cum illum dicta? Omnis nisi beatae minus soluta reprehenderit, unde non, iusto eligendi pariatur porro culpa magni maiores voluptatem est praesentium consectetur magnam, laborum et libero inventore minima. Quas earum officiis blanditiis et? Expedita commodi illum fugit repudiandae eligendi quisquam illo iure. Tempora sed, sunt voluptate quis facilis dolorem saepe aspernatur maxime totam provident similique quod debitis placeat praesentium dolore iusto deleniti pariatur quae molestias architecto nam a eius? Officia est sapiente incidunt minima ea?</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-card color="transparent" width="85%" flat class="d-flex justify-space-around">
+        <v-col cols="12" md="6" class="d-flex justify-space-around">
+          <v-card class="d-inline justify-space-around col-12">
+            <v-row style="background: red" class="elevation-2">
+              <v-col>
+                <v-card-title
+                  class="title font-weight-bold text-break text-center white--text"
+                >HOME HEALTH CARE</v-card-title>
+              </v-col>
+              <v-col cols="2">
+                <v-card-title class="title font-weight-bold text-break text-center white--text">VS</v-card-title>
+              </v-col>
+              <v-col>
+                <v-card-title
+                  class="title font-weight-bold text-break text-center white--text"
+                >HOME CARE</v-card-title>
+              </v-col>
+            </v-row>
+            <v-spacer />
+            <v-row>
+              <v-col>
+                <ul>
+                  <li class="subtitle__widu font-weight-medium">Single-line item</li>
+                  <li class="subtitle__widu font-weight-medium">Single-line item</li>
+                  <li class="subtitle__widu font-weight-medium">Single-line item</li>
+                </ul>
+              </v-col>
+              <v-col cols="2"></v-col>
+              <v-col>
+                <ul>
+                  <li class="subtitle__widu font-weight-medium">Single-line item</li>
+                  <li class="subtitle__widu font-weight-medium">Single-line item</li>
+                  <li class="subtitle__widu font-weight-medium">Single-line item</li>
+                </ul>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-img src="@/assets/image/Image 15.png" width="562px" height="506px" contain />
+        </v-col>
+      </v-card>
     </v-row>
   </v-container>
 </template>
