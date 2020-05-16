fc3895b4a0e9b5f2270443140168ce78d812f8b8
Weynand <wamaerkabor@gmail.com>
2020-05-17 01:21:53 +0700

    Create base-material-button

---
+++ src/components/base/MaterialButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn
+    :outlined="test"
+    @mouseover="test = true"
+    @mouseleave="test = false"
+    rounded
+    depressed
+    class="elevation-1 font-weight-bold"
+    color="primary"
+  >
+    <slot />
+  </v-btn>
+</template>
+<script>
+export default {
+  name: "MaterialButton",
+  data: () => ({
+    test: false
+  }),
+  props: {
+    text: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
