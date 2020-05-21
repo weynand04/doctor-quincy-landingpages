f3beba8aeb981369ad8845683315acd698f4906b
Weynand <wamaerkabor@gmail.com>
2020-05-21 16:54:00 +0700

    Create new commponent judul

--- src/views/homepage/components/base/Judul.vue
+++ src/views/homepage/components/base/Judul.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="display-1 font-weight-light col-12 text-left pa-0 text-break">
-    <h5 class="font-weight-bold orange--text">
+    <h5 class="font-weight-bold primary--text">
       {{ subjudul }}
       <template v-if="text">
         <span class="subtitle-1" v-text="text" />
