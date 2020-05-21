18c034b99f501e2bbb972cc13fb12680b9d6e14f
Weynand <wamaerkabor@gmail.com>
2020-05-21 16:53:44 +0700

    edit pages ondevlop to underconstract

---
+++ src/views/homepage/pages/UnderConstruct.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container style="height: 40rem" class="error-page" fill-height tag="section">
+    <v-row>
+      <v-col cols="12">
+        <subjudul
+          class="text-uppercase text-center"
+          subjudul="Sorry, This page is under Construction"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import subjudul from "../components/base/Judul";
+
+export default {
+  name: "UnderConstruct",
+  data: () => ({}),
+  components: {
+    subjudul
+  }
+};
+</script>
