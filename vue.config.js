79778f8fbf8935f4751dd71d8e81ee6df6bca0f6
Weynand <wamaerkabor@gmail.com>
2020-05-25 09:04:08 +0700

    modif vue config untuk run build local

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ?
+//     '/drquincy/' : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/drquincy/' : '/'
+  devServer: {
+    disableHostCheck: true,
+  },
+  transpileDependencies: ['vuetify'],
 }
