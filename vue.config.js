72dcb6bc708172aaf737eddd495b63155b658ce3
Weynand <wamaerkabor@gmail.com>
2020-05-27 10:08:22 +0700

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production' ?
-//     '/drquincy/' : '/'
-// }
+ module.exports = {
+   publicPath: process.env.NODE_ENV === 'production' ?
+     '/drquincy/' : '/'
+ }
 
-module.exports = {
-  devServer: {
-    disableHostCheck: true,
-  },
-  transpileDependencies: ['vuetify'],
-}
+ // module.exports = {
+ //   devServer: {
+ //     disableHostCheck: true,
+ //   },
+ //   transpileDependencies: ['vuetify'],
+ // }
