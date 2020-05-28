8866b5854f14d8db70d99cec03193850506230a1
Weynand <wamaerkabor@gmail.com>
2020-05-28 22:03:31 +0700

    Add commponet BannerTop coba pada halaman Help

---
+++ src/views/homepage/components/base/BannerTop.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="bannertop">
+    <ContainerWidu>
+      <v-img
+        :src="image"
+        :position="position"
+        max-height="500"
+        transition="scale-transition"
+        contain
+      >
+        <v-row class="d-flex align-content-center fill-height img-opacity">
+          <v-col v-if="right = right" cols="6"></v-col>
+          <v-col cols="12" md="6">
+            <Subjudul :judul="judul" />
+            <p class="body-1">{{desc}}</p>
+            <div>
+              <slot />
+            </div>
+          </v-col>
+          <v-col v-if="left = left" cols="6"></v-col>
+        </v-row>
+      </v-img>
+    </ContainerWidu>
+  </div>
+</template>
+<script>
+import ContainerWidu from "./ContainerWidu";
+import Subjudul from "./Judul";
+
+export default {
+  name: "bannertop",
+  components: {
+    ContainerWidu,
+    Subjudul
+  },
+  props: {
+    judul: {
+      type: String,
+      default: "Judul"
+    },
+    desc: {
+      type: String,
+      default: ""
+    },
+    image: {
+      type: String,
+      default: require("@/assets/image/svg/new-12.svg")
+    },
+    position: {
+      type: String,
+      default: "right center"
+    },
+    left: {
+      type: Boolean,
+      default: true
+    },
+    right: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@media only screen and (max-width: 600px) {
+  .img-opacity {
+    background-color: rgba(255, 255, 255, 0.6);
+  }
+}
+@media only screen and (max-width: 960px) {
+  .img-opacity {
+    background-color: rgba(255, 255, 255, 0.4);
+  }
+}
+</style>
--- src/views/homepage/pages/Help.vue
+++ src/views/homepage/pages/Help.vue
@@ -1,88 +1,46 @@
 <template>
   <div id="help" tag="section">
-    <template v-for="(section, index) in sections">
-      <ContainerWidu :key="index">
-        <v-row
-          v-for="(item, index) in section.bannerTop"
-          :key="index"
-          no-gutters
-          class="pl-sm-6 pl-md-2 pl-lg-3"
-        >
-          <v-col cols="12" md="4" sm="4" lg="4" xl="4" align-self="center">
-            <subjudul :judul="item.judul" />
-            <p>{{item.teks}}</p>
-            <v-btn rounded color="primary" class="elevation-0">Login</v-btn>
-            <v-btn rounded outlined color="primary">Register</v-btn>
-          </v-col>
-          <v-col cols="12" md="8" sm="8" lg="8" xl="8" class="mt-5">
-            <v-img
-              :src="item.gambar"
-              max-height="706"
-              max-width="662"
-              aspect-ratio="1.6"
-              position="center right 20%"
-              contain
-            />
-          </v-col>
-        </v-row>
-      </ContainerWidu>
-
-      <!-- Component banner bottom -->
-    </template>
+    <!-- Component banner top -->
+    <div v-for="(item, index) in bannerTop" :key="index">
+      <Bannertop
+        :judul="item.judul"
+        :desc="item.teks"
+        :image="item.gambar"
+        btn="btn"
+        btnName="Register"
+        left="left"
+      >
+        <v-btn rounded color="primary" max-width="100" height="40" class="elevation-0 mx-2">Register</v-btn>
+        <v-btn rounded color="primary" max-width="100" height="40" class="elevation-0 mx-2">Login</v-btn>
+      </Bannertop>
+    </div>
+    <ContainerWidu>
+      <v-card flat height="400"></v-card>
+    </ContainerWidu>
+    <!-- Component banner bottom -->
     <Bannerbot />
   </div>
 </template>
 
 <script>
 import Bannerbot from "../components/base/BannerBot";
-import Subjudul from "../components/base/Judul";
 import ContainerWidu from "../components/base/ContainerWidu";
+import Bannertop from "../components/base/BannerTop";
 
 export default {
   name: "Help",
   components: {
+    Bannertop,
     ContainerWidu,
-    Subjudul,
     Bannerbot
   },
   data: () => ({
-    sections: [
+    bannerTop: [
       {
-        bannerTop: [
-          {
-            judul: "Lorem ipsum dolor sit.",
-            teks:
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quas quasi cupiditate id? Animi ducimus laborum neque id expedita, possimus tempore, hic nam inventore enim itaque consequatur corrupti commodi quisquam!",
-            gambar: require("@/assets/image/Group 1031.png")
-          }
-        ]
-      },
-      {
-        contentSatu: [
-          {
-            subjudul: "Stok Card Widu",
-            cards: [
-              {
-                title: "Berpengalaman",
-                text:
-                  "Pengalaman sangat penting bagi kami. Penyedia kami memiliki pengalaman yang sudah bertahun-tahun. Karena perawatan kesehatan yang berkualitas mulai dengan dokter yang berkualitas.",
-                image: require("@/assets/image/Image 19.png")
-              },
-              {
-                title: "Berlisensi dan Bersertifikat",
-                text:
-                  "Penyedia kami bersertifikat IDI, mereka adalah praktisi berlisensi Negara dan telah memenuhi persyaratan. Dokter kami terus mengukur kompetensi praktek dokter di setiap spesialisasinya.",
-                image: require("@/assets/image/Mask Group 9.png")
-              },
-              {
-                title: "Perlindungan & Keamanan",
-                text:
-                  "Informasi pribadi dan riwayat kesehatan kamu dilindungi dengan aman. Semua pertemuan diarsipkan secara digital dengan tingkat keamanan tertinggi dan hanya dapat diakses oleh kamu dan dokter-nya.",
-                image: require("@/assets/image/Mask Group 6.png")
-              }
-            ]
-          }
-        ]
+        judul: "Lorem ipsum dolor sit.",
+        teks:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quas quasi cupiditate id? Animi ducimus laborum neque id expedita, possimus tempore, hic nam inventore enim itaque consequatur corrupti commodi quisquam!",
+        gambar: require("@/assets/image/gambar lain-11.png")
       }
     ]
   })
