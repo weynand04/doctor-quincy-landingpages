d250ff4838cd383d032f239e61d00cf8a1c0e6cc
Weynand <wamaerkabor@gmail.com>
2020-05-28 07:37:05 +0700

    initial commit 2.0

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,3 @@
    publicPath: process.env.NODE_ENV === 'production' ?
      '/drquincy/' : '/'
  }
-
- // module.exports = {
- //   devServer: {
- //     disableHostCheck: true,
- //   },
- //   transpileDependencies: ['vuetify'],
- // }
