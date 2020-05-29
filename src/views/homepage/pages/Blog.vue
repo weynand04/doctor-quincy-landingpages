803d301609f6240fa10c8563fd23589a15e1b27d
Weynand <wamaerkabor@gmail.com>
2020-05-29 18:15:51 +0700

    add komponen bannerTop ke pg event dan mediaberita

--- src/views/homepage/pages/Blog.vue
+++ src/views/homepage/pages/Blog.vue
@@ -2,26 +2,25 @@
   <div id="blog" tag="section">
     <div v-for="(section, insec) in sections" :key="insec">
       <!-- bannerTop -->
-      <ContainerWidu v-for="(item, index) in section.bannerTop" :key="index">
-        <v-row no-gutters class="pl-sm-6 pl-md-2 pl-lg-3">
-          <v-col cols="12" md="6" sm="6" lg="6" xl="6" align-self="center">
-            <subjudul :judul="item.judul" />
-            <p class="body-1">{{item.teks}}</p>
-            <v-btn rounded max-width="100" height="40" color="primary">Login</v-btn>
-            <v-btn rounded outlined max-width="100" height="40" color="primary">Register</v-btn>
-          </v-col>
-          <v-col cols="12" md="6" sm="6" lg="6" xl="6">
-            <v-img
-              :src="item.gambar"
-              max-height="848"
-              max-width="832"
-              aspect-ratio="1.2"
-              position="center right"
-              contain
-            />
-          </v-col>
-        </v-row>
-      </ContainerWidu>
+      <div v-for="(item, index) in section.bannerTop" :key="index">
+        <Bannertop
+          :judul="item.judul"
+          :desc="item.teks"
+          :image="item.gambar"
+          btn="btn"
+          btnName="Register"
+          left="left"
+        >
+          <v-btn
+            rounded
+            color="primary"
+            max-width="100"
+            height="40"
+            class="elevation-0 mx-2"
+          >Register</v-btn>
+          <v-btn rounded color="primary" max-width="100" height="40" class="elevation-0 mx-2">Login</v-btn>
+        </Bannertop>
+      </div>
 
       <!-- content bottom -->
       <ContainerWidu
@@ -54,12 +53,14 @@
 <script>
 import ContainerWidu from "../components/base/ContainerWidu";
 import Subjudul from "../components/base/Judul";
+import Bannertop from "../components/base/BannerTop";
 
 export default {
   name: "Help",
   components: {
     ContainerWidu,
-    Subjudul
+    Subjudul,
+    Bannertop
   },
   data: () => ({
     sections: [
